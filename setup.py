a187e2cdf3066d2d58943acfa794ca1dacc56cb7
Carson Ip <carsonip715@gmail.com>
2019-03-26 12:44:20 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.6.13',
+      version='10.6.13',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[],
